7857d663d063eaf12e2960d68b4245d574babfe1
Hridyanshu1934 <hridyanshukhattar@icloud.com>
2026-03-08 12:50:38 +0530

    Day1

---
+++ video39To40.js
@@ -0,0 +1,33 @@
+/** Create three variables:
+name = "Hridyanshu"
+age = 22
+isStudent = true
+Print all of them using console.log. **/
+let name = "Hridyanshu";
+let age = 22;
+let isStudent = true;
+console.log(name,age,isStudent);
+/*** Create a variable score with value 10.
+Print it.
+Then update the value to 25 and print it again. */
+let score = 10;
+console.log(score);
+score = 25;
+console.log(score);
+/**Create variables:
+name = "Rahul"
+age = 20
+isLoggedIn = false
+Print the data type of each variable using typeof. */
+let name1 = "Rahul";
+let age1 = 20;
+let isLoggedIn = false;
+console.log(typeof(isLoggedIn));
+console.log(typeof(name1));
+console.log(typeof(age1));
+/**Declare multiple variables in one line:
+let x = 5, y = 10, z = 15
+Print the sum of x, y, and z. */
+
+let x = 5, y = 10, z = 15;
+console.log(x+y+z);
